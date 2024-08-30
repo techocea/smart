64f20c84624ed0e6852aecb0043e6f1e39ae7554
techocea <dunsfordbright@gmail.com>
2024-08-30 09:49:06 +0530

    few changes

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://achievers-client.vercel.app"]
     credentials: true,
   })
 );
--- frontend/src/components/services.jsx
+++ frontend/src/components/services.jsx
@@ -2,7 +2,7 @@ import React,{useEffect} from "react";
 
 import { Link } from "react-router-dom";
 
-import Map from "./Map";
+import Map from "./map";
 
 import { ServiceData } from "../utils/constants";
 
